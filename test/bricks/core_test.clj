3636305bad5ce652aa3de2a8a8305ef9a9d7caed
Meike Mertsch <nmmertsch@gmail.com>
2015-11-29 21:02:03 +0100

    removed crap

--- test/bricks/core_test.clj
+++ test/bricks/core_test.clj
@@ -4,10 +4,3 @@
             [expectations :refer :all]))
 
 (expect 86 (color-id "light bluish gray"))
-
-(expect :avg_price (in (keys (dissoc (html-get "/items/part/3795/price"
-                                               {:type       "part"
-                                                :no         "3795"
-                                                :color_id   86
-                                                :guide_type "sold"})
-                                     :price_detail))))
