6f38bb4fa338b2a7e759052dbdbe430f9c44ec3d
Meike Mertsch <nmmertsch@gmail.com>
2015-11-29 13:21:18 +0100

    Made OAuth work, though flaky

--- src/bricks/constants.clj
+++ src/bricks/constants.clj
@@ -1,3 +1,4 @@
 (ns bricks.constants)
 
 (def base-url "https://api.bricklink.com/api/store/v1")
+(def transform-to-keywords true)
--- test/bricks/core_test.clj
+++ test/bricks/core_test.clj
@@ -1,11 +1,46 @@
 (ns bricks.core-test
   (:require [expectations :refer :all]
-            [bricks.core :as core]))
+            [bricks.core :as core]
+            [oauth.client :as oauth]
+            [clj-http.client :as http]
+            [cheshire.core :as json]
+            [bricks.constants :as const]))
+
+(def oauth-secrets (json/parse-string (slurp "secrets") const/transform-to-keywords))
+
+(def consumer (oauth/make-consumer (:key oauth-secrets)
+                                   (:secret oauth-secrets)
+                                   nil
+                                   nil
+                                   nil
+                                   :hmac-sha1))
+
+(defn ->header [site]
+  (let [creds (oauth/credentials consumer
+                                 (:token oauth-secrets)
+                                 (:token-secret oauth-secrets)
+                                 :GET
+                                 (str const/base-url site))]
+    {"Authorization" (str "OAuth realm=\"\",oauth_version=\"1.0\",oauth_consumer_key=\"" (:oauth_consumer_key creds)
+                          "\",oauth_token=\"" (:oauth_token creds)
+                          "\",oauth_timestamp=\"" (:oauth_timestamp creds)
+                          "\",oauth_nonce=\"" (:oauth_nonce creds)
+                          "\",oauth_signature_method=\"HMAC-SHA1\",oauth_signature=\"" (:oauth_signature creds)
+                          "\"")}))
+
+(defn test-get [site]
+  (core/html-get site {:headers (->header site)}))
 
 
-(defn headers []
- {"Authorization" (slurp "secret")})
-
-(println (core/html-get "/inventories" {:headers (headers)}))
+(println (test-get "/colors"))
 
 
+(defn test-get-params []
+  (let [params {:email "blah@blah.com"}
+        credentials (oauth/credentials consumer
+                                       nil
+                                       nil
+                                       :GET
+                                       "https://api.context.io/lite/users"
+                                       params)]
+    (http/get "https://api.context.io/lite/users" {:query-params (merge credentials params)})))
