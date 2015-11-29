cd14e591e094d92f90fdf25ba09da8b20fca730a
Meike Mertsch <nmmertsch@gmail.com>
2015-11-29 14:39:56 +0100

    things in new places

---
+++ src/bricks/auth.clj
@@ -0,0 +1,35 @@
+(ns bricks.auth
+  (:require [bricks.constants :as const]
+            [oauth.client :as oauth]
+            [cheshire.core :as json]))
+
+(def oauth-secrets (json/parse-string (slurp "secrets") const/transform-to-keywords))
+
+(defn retry [f max]
+  (loop [i 0
+         result nil]
+    (if (or (>= i max)
+            (not= nil result))
+      [i result]
+      (recur (inc i)
+             (f)))))
+
+(def consumer (oauth/make-consumer (:key oauth-secrets)
+                                   (:secret oauth-secrets)
+                                   nil
+                                   nil
+                                   nil
+                                   :hmac-sha1))
+
+(defn ->header [site]
+  (let [creds (oauth/credentials consumer
+                                 (:token oauth-secrets)
+                                 (:token-secret oauth-secrets)
+                                 :GET
+                                 (str const/base-url site))]
+    {"Authorization" (str "OAuth realm=\"\",oauth_version=\"1.0\",oauth_consumer_key=\"" (:oauth_consumer_key creds)
+                          "\",oauth_token=\"" (:oauth_token creds)
+                          "\",oauth_timestamp=\"" (:oauth_timestamp creds)
+                          "\",oauth_nonce=\"" (:oauth_nonce creds)
+                          "\",oauth_signature_method=\"HMAC-SHA1\",oauth_signature=\"" (:oauth_signature creds)
+                          "\"")}))
---
+++ src/bricks/html.clj
@@ -0,0 +1,25 @@
+(ns bricks.html
+  (:gen-class)
+  (:require [bricks.auth :as auth]
+            [clj-http.client :as client]
+            [bricks.constants :as const]
+            [cheshire.core :as json]))
+
+(defn ->data [html-answer]
+  (->> html-answer
+       :body
+       (#(json/parse-string % const/transform-to-keywords))
+       :data))
+
+(defn html-get
+  ([url] (html-get url nil))
+  ([url params] (client/get (str const/base-url url) (merge {:headers (auth/->header url)}) params)))
+
+(defn html-post [url params]
+  (client/post (str const/base-url url) params))
+
+
+
+
+
+(println (auth/retry #(->data (html-get "/inventories")) 10))
