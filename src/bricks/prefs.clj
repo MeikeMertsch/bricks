b516dcc27cf28799c9f9556327d7ac794b23e55d
Meike Mertsch <nmmertsch@gmail.com>
2015-11-29 16:26:40 +0100

    making it possible to reload local stuff

--- src/bricks/prefs.clj
+++ src/bricks/prefs.clj
@@ -1,1 +1,11 @@
-(ns bricks.prefs)
+(ns bricks.prefs
+  (:require [cheshire.core :as json]
+            [bricks.html :as html]))
+
+(defn reload-colors []
+  (->> (html/html-get "/colors")
+       (filter #(not= "Modulex" (:color_type %)))
+       (map #(dissoc % :color_code))
+       (map #(update-in % [:color_name] clojure.string/lower-case))
+       json/generate-string
+       (spit "colors")))
