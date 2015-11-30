d06669c65b4426b2ab8aad1282f5c683c8e58a98
Meike Mertsch <nmmertsch@gmail.com>
2015-11-30 20:48:14 +0100

    added color

---
+++ src/bricks/color.clj
@@ -0,0 +1,18 @@
+(ns bricks.color
+  (:require [bricks.constants :as const]
+            [bricks.html :as html]
+            [cheshire.core :as json]
+            [com.rpl.specter :as specter]))
+
+(def colors (->> (slurp "resources/colors")
+                 (#(json/parse-string % const/transform-to-keywords))))
+
+(defn color-id [name]
+  (let [colors-->id [specter/ALL #(= (clojure.string/lower-case name) (:color_name %)) :color_id]
+        id (peek (specter/select colors-->id colors))]
+    (if id id (throw (Exception. "The color isn't recognized")))))
+
+(defn known-color? [part color-id]
+  (->> (html/html-get (str "/items/part/" part "/colors"))
+       (filter #(= color-id (:color_id %)))
+       ((complement empty?))))
--- src/bricks/functions.clj
+++ src/bricks/functions.clj
@@ -1,19 +1,10 @@
 (ns bricks.functions
-  (:require [bricks.constants :as const]
+  (:require [bricks.color :as color]
             [bricks.html :as html]
-            [bricks.io :as io]
-            [cheshire.core :as json]
-            [com.rpl.specter :as specter]))
+            [bricks.io :as io]))
 
-(def colors (->> (slurp "resources/colors")
-                 (#(json/parse-string % const/transform-to-keywords))))
 
-(defn color-id [name]
-  (let [colors-->id [specter/ALL #(= (clojure.string/lower-case name) (:color_name %)) :color_id]
-        id (peek (specter/select colors-->id colors))]
-    (if id id (throw (Exception. "The color isn't recognized")))))
-
-(defn price [number color-id]
+(defn avg_price [number color-id]
   (->> (html/html-get (str "/items/part/" number "/price")
                       {:type       "part"
                        :no         number
@@ -21,42 +12,33 @@
                        :guide_type "sold"})
        :avg_price))
 
-
-
 (defn parse-upload-instructions [line]
   (let [[part qty color] (clojure.string/split line #";")
         ->int #(int (bigint %))]
     (try
-      [line part (->int qty) (color-id color)]
+      [line part (->int qty) (color/color-id color)]
 
       (catch Exception e
         (let [log (format "%s --> skipped: %s" line e)]
           [log])))))
-
-(defn known-color? [part color-id]
-  (->> (html/html-get (str "/items/part/" part "/colors"))
-       (filter #(= color-id (:color_id %)))
-       ((complement empty?))))
-
 
 (defn validate-instructions
   ([log] [log])
   ([line part quantity color-id]
    (let [log #(format "%s --> skipped: %s" line %)]
      (try
-       (if (known-color? part color-id)
+       (if (color/known-color? part color-id)
          [line part quantity color-id]
          [(log "color is not known for that part")])
        (catch Exception e
          [(log e)])))))
-
 
 (defn ->items [[_ part quantity color-id]]
   {:item           {:no   part
                     :type "part"}
    :color_id       color-id
    :quantity       quantity
-   :unit_price     (price part color-id)
+   :unit_price     (avg_price part color-id)
    :new_or_used    "N"
    :description    ""
    :remarks        ""
@@ -76,7 +58,8 @@
 
 
 (defn upload-inventories [file]
-  (->> (io/read-lines file parse-upload-instructions)
+  (->> parse-upload-instructions
+       (io/read-lines file)
        (map #(apply validate-instructions %))
        (#(if (empty? (filter (fn [item] (= 1 (count item))) %))
           (->> (map ->items %)
