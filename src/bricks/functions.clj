d074b9cd0431eaa86713a26a3927e6a53d4f91d5
Meike Mertsch <nmmertsch@gmail.com>
2015-12-03 07:46:54 +0100

    create upload instructions for items in set

--- src/bricks/functions.clj
+++ src/bricks/functions.clj
@@ -67,8 +67,31 @@
    :tier_quantity3 0
    :tier_price3    0})
 
-(defn ->upload-instruction [item]
-  )
+
+(defn ->upload-instruction [unit-price
+                            {quantity              :quantity
+                             color-id              :color_id
+                             {part :no type :type} :item}]
+  {:item           {:no   part
+                    :type type}
+   :color_id       color-id
+   :quantity       quantity
+   :unit_price     unit-price
+   :new_or_used    "N"
+   :description    ""
+   :remarks        ""
+   :bulk           1
+   :is_retain      false
+   :is_stock_room  true
+   :stock_room_id  "B"
+   :my_cost        0.0
+   :sale_rate      0
+   :tier_quantity1 0
+   :tier_price1    0
+   :tier_quantity2 0
+   :tier_price2    0
+   :tier_quantity3 0
+   :tier_price3    0})
 
 
 
@@ -119,27 +142,31 @@
                println)
           (io/write-lines file (map first %))))))
 
-(defn part-out-set [set-no quantity delete-file update-file]
+(defn part-out-set [set-no quantity delete-file update-file margin-set-price]
   (let [inventory (multiply-set (part-out set-no) quantity)
         deletions (io/parse-lines-with-f delete-file parse-deletions)
         updates (io/parse-lines-with-f update-file parse-upload-instructions)]
     (-> (delete-in-set inventory deletions)
-        (update-in-set updates))
+        (update-in-set updates)
+        (#(let [sum-parts (count-parts %)
+                price (* margin-set-price quantity)
+                unit-price (/ price sum-parts)]
+           (map (partial ->upload-instruction unit-price) %)))
 
-    ; Load set inventory
-    ; Multiply by quantity
+        ; Load set inventory
+        ; Multiply by quantity
 
-    ; Parse Delete Instructions
-    ; Parse Update Instructions
+        ; Parse Delete Instructions
+        ; Parse Update Instructions
 
-    ; Delete from set inventory
-    ; Update in set inventory
+        ; Delete from set inventory
+        ; Update in set inventory
 
-    ; Calculate Sum of parts
+        ; Calculate Sum of parts
 
-    ; Create upload instructions
-    ; POST
-    ))
+        ; Create upload instructions
+        ; POST
+        )))
 
 ; TODO:
 (defn update-inventories [file stockroom]
--- test/bricks/functions_test.clj
+++ test/bricks/functions_test.clj
@@ -43,4 +43,21 @@
   (expect 23 (count-parts set)))
 
 (let [set (slurp-res "set-inventory")]
-(expect nil (->upload-instruction (first set))))
+(expect {:remarks "",
+         :description "",
+         :is_stock_room true,
+         :tier_price3 0,
+         :sale_rate 0,
+         :unit_price 0.1,
+         :tier_price2 0,
+         :bulk 1,
+         :item {:no "3005", :type "PART"},
+         :tier_quantity2 0,
+         :stock_room_id "B",
+         :tier_price1 0,
+         :tier_quantity1 0,
+         :tier_quantity3 0,
+         :my_cost 0.0,
+         :color_id 1,
+         :new_or_used "N", :quantity 2, :is_retain false}
+        (->upload-instruction 0.10 (first set))))
