57de03ebe87ca7592fc23413ccf560afdf68fcd9
Meike Mertsch <nmmertsch@gmail.com>
2015-12-05 19:20:26 +0100

    added more files about the operations

--- test/bricks/functions_test.clj
+++
@@ -1,8 +0,0 @@
-(ns bricks.functions-test
-  (:require [bricks.functions :refer :all]
-            [expectations :refer :all]))
-
-(expect "0.55" (divide 0.545M 1))
-(expect "0.54" (divide 0.544M 1))
-(expect "1.50" (divide 3 2M))
-(expect "2.00" (divide 8 4M))
