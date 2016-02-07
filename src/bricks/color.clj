93d0da7f7918527137e7d4fdcbf464129e42b8f9
‘MeikeMertsch’ <nmmertsch@gmail.com>
2016-02-07 11:29:39 +0100

    added short name func for colors

--- src/bricks/color.clj
+++ src/bricks/color.clj
@@ -12,12 +12,32 @@
         id (peek (specter/select colors-->id colors))]
     (if id id (throw (Exception. (format "The color %s isn't recognized" name))))))
 
-(defn color-name [color_id]
-  (let [colors-->name [specter/ALL #(= color_id (:color_id %)) :color_name]
+(defn color-name [color-id]
+  (let [colors-->name [specter/ALL #(= color-id (:color_id %)) :color_name]
         name (peek (specter/select colors-->name colors))]
-    (if name name (throw (Exception. (format "The color %s isn't recognized" color_id))))))
+    (if name name (throw (Exception. (format "The color %s isn't recognized" color-id))))))
 
 (defn known-color? [part color-id]
   (->> (html/html-get (str "/items/part/" part "/colors"))
        (filter #(= color-id (:color_id %)))
        ((complement empty?))))
+
+(defn replace-all-patterns [coll string]
+  (loop [replacements coll
+         s string]
+    (if (empty? replacements)
+      s
+      (recur (rest replacements)
+             (let [[part abb] (first replacements)]
+               (clojure.string/replace s (re-pattern part) abb))))))
+
+(defn short-color-name [color-id]
+  (->> (color-name color-id)
+       (replace-all-patterns [["bright" "B"]
+                              ["light" "L"]
+                              ["reddish" "R"]
+                              ["dark" "D"]
+                              ["medium" "M"]
+                              ["trans" "T"]
+                              ["flat" "F"]
+                              ["pearl" "P"]])))
