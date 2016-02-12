0fabaa06596bd30b6f79e8eaea397b49bb18a2ff
‘MeikeMertsch’ <nmmertsch@gmail.com>
2016-02-12 13:13:27 +0100

    added temporary reading from file instead of api

--- src/bricks/html.clj
+++ src/bricks/html.clj
@@ -37,3 +37,8 @@
                                             10))
                    url
                    params)))
+
+
+(defn temp [path]
+  (-> (slurp path)
+      (json/parse-string const/transform-to-keywords)))
