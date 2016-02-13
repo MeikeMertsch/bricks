9fe20dbe588724981c399e1d04817ef9e64a48de
‘MeikeMertsch’ <nmmertsch@gmail.com>
2016-02-13 14:28:39 +0100

    added checklist for 41040

--- src/bricks/functions.clj
+++ src/bricks/functions.clj
@@ -108,8 +108,9 @@
 ;   (println (create-checklist "5994-1" 75))
 ;(println (create-checklist "75104-1" 1))
 ;(println (create-checklist "41044-1" 10))
+(println (create-checklist "41040-1" 3))
+
 #_(
-    (println (create-checklist "41044-1" 10))
     (println (create-checklist "41040-1" 3))
     (println (create-checklist "41102-1" 7))
     (println (create-checklist "41545-1" 1))
